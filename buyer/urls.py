a1d04283dc4e173de087413b624d8b3e810d2d5a
hashmimdfaiz <hashmimdfaiz077@gmail.com>
2023-05-15 13:06:30 +0530

    Initial commit

--- buyer/urls.py
+++ buyer/urls.py
@@ -28,3 +28,4 @@ urlpatterns = [
     path('logout/',Homepage.as_view(),name = 'buyer_logout'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
