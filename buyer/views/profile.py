0e4f46b8c615d959a14c5216325731465403701c
Disha Arora <dishaarora1996@gmail.com>
2023-06-24 23:49:22 +0530

    profile api in progress

--- buyer/views/profile.py
+++ buyer/views/profile.py
@@ -1,12 +1,37 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from django.views import View
+from django.contrib import messages
+from buyer.models.registrations import User
 
 # Create your views here.
 class Profile(View):
     def get(self,request):
-        return render(request,'buyer/profileupdate.html')
+        return render(request,'buyer/profile_update.html')
     def post(self,request):
-        return render(request,'buyer/profileupdate.html')
+        first_name = request.POST.get('first_name').capitalize()
+        last_name = request.POST.get('last_name').capitalize()
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        city = request.POST.get('city')
+        gender = request.POST.get('gender')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+        print(first_name + ' ' + last_name + ' ' + email)
+       
+        if password1 != password2:
+            print("Not Match")
+            messages.error(request, "Password does not match")
+            return render('buyer_profile')
+        else:
+            customer = request.session.get('customer')
+            try:
+                user = User.objects.get(id=customer)
+            except User.DoesNotExist:
+                messages.error("User not found")
+                return redirect('login')
+            
+        
+        
 
 class Help(View):
     def get(self,request):
--- grabitfy/settings.py
+++ grabitfy/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from django.contrib.messages import constants as messages
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -118,6 +120,14 @@ USE_I18N = True
 
 USE_TZ = True
 
+
+MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-secondary',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+ }
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
